d6bfcf306741adbab3d4328aca243fc470d27d9d
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-07 11:57:39 +0600

    CF 800 rating problem CapsLock

---
+++ CF_800RatedProblem_CapsLock.cpp
@@ -0,0 +1,19 @@
+///Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>> s;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(islower(s[0])){
+            s[0] = toupper(s[0]);
+        }
+        if(isupper(s[i+1])){
+            s[i+1] = tolower(s[i+1]);
+        }
+    }
+    cout<< s << endl;
+    return 0;
+}
