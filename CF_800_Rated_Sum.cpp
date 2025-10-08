82f71cd5538f4adec8fb60c0ed4adec5c5e78bd9
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-08 20:44:33 +0600

    CodeForces 800 rating Sum problem *implementation

---
+++ CF_800_Rated_Sum.cpp
@@ -0,0 +1,21 @@
+///Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, t;
+    cin>> t;
+    while(t--){
+        cin >> a >> b >> c;
+        if(a + b == c || a + c == b || b + c == a)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
