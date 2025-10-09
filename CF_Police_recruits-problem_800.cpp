de2771da35d60a7454f4fd8d03ee0d362c1662bc
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-09 06:02:47 +0600

    CodeForces *800 rated Police Recurits Problem

---
+++ CF_Police_recruits-problem_800.cpp
@@ -0,0 +1,31 @@
+///Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int untreated = 0, police = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if(x == -1)
+        {
+            if(police > 0)
+            {
+                police--;
+            }
+            else
+            {
+                untreated++;
+            }
+        }
+        else
+        {
+            police += x;
+        }
+    }
+    cout << untreated << endl;
+    return 0;
+}
