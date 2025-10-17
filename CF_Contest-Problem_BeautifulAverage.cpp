bfc6b7c4dfdf54e700846192e19ba97f8cd0fb3c
Sajid <c243042@ugrad.iiuc.ac.bd>
2025-10-17 21:34:11 +0600

    CF Div. 3 contest A solve Beautiful average

---
+++ CF_Contest-Problem_BeautifulAverage.cpp
@@ -0,0 +1,20 @@
+/// Bismillahir Rahmanir Rahim
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        int ans = *max_element(v.begin(), v.end());
+        cout << ans << endl;
+    }
+    return 0;
+}
